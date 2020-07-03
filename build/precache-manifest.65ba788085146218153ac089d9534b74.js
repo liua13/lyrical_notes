a1b12da8efee775d2bf56d2918e17ad5fbb2efeb
liua13 <liua1@bxscience.edu>
2020-07-03 13:55:29 -0400

    finished project

---
+++ build/precache-manifest.65ba788085146218153ac089d9534b74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1408ffd604d183406c5fca4cc42b77d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed22dca52a6c18dc8987",
+    "url": "/static/css/main.57681b86.chunk.css"
+  },
+  {
+    "revision": "22996450d020003cf034",
+    "url": "/static/js/2.281e9c81.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.281e9c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed22dca52a6c18dc8987",
+    "url": "/static/js/main.936c7660.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "edd59a5ce77c9032da5b337b96d12767",
+    "url": "/static/media/favicon.edd59a5c.png"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,81 @@
+import express from "express";
+import path from "path";
+import bodyparser from "body-parser";
+import request from "request";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8888;
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors());
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+let client_id = process.env.client_id;
+let client_secret = process.env.client_secret;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const requestAuthorization = () => {
+  // your application requests authorization
+  return {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+    form: {
+      grant_type: "client_credentials",
+    },
+    json: true,
+  };
+};
+
+app.post("/api/getTrackByGenre", (req, res) => {
+  let authOptions = requestAuthorization();
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      // generate random number for offset [0, 1950] and limit [15, 20]
+      let { genre } = req.body;
+      let randomOffset = Math.floor(Math.random() * 1950);
+      let randomLimit = Math.floor(Math.random() * 16) + 10;
+
+      // console.log("offset: ", randomOffset);
+      // console.log("limit: ", randomLimit);
+
+      // use the access token to access the Spotify Web API
+      let token = body.access_token;
+      let options = {
+        url: "https://api.spotify.com/v1/search",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+        qs: {
+          q: `genre: ${genre}`,
+          type: "track",
+          offset: randomOffset,
+          limit: randomLimit,
+        },
+        json: true,
+      };
+
+      request.get(options, function (error, response, body) {
+        res.send(body.tracks.items);
+      });
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
