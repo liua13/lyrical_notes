2f0c57ab7f4314e5c4cf9d71082e176f232f6ae4
liua13 <liua1@bxscience.edu>
2020-07-06 22:51:20 -0400

    updated genres

---
+++ build/precache-manifest.ee0b327b6ffeeddeece5732b6096908a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea52ae6fbfedd720db1c31555b086d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db49754cef8a004930c1",
+    "url": "/static/css/main.92a312c4.chunk.css"
+  },
+  {
+    "revision": "1cd1669857bd01827191",
+    "url": "/static/js/2.62ee1cf6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62ee1cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db49754cef8a004930c1",
+    "url": "/static/js/main.90612e8f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "edd59a5ce77c9032da5b337b96d12767",
+    "url": "/static/media/favicon.edd59a5c.png"
+  }
+]);
